8d1a2f4f600ea41bec1d28fd91a619b8db37a130
James Pollack <james.b.pollack@gmail.com>
2023-05-05 01:52:35 -0700

    works at least for the nextbus aka umoiq api

--- index.mjs
+++ index.mjs
@@ -2,100 +2,63 @@ import axios from "axios";
 import { URL } from "url";
 
 export const handler = async (event) => {
+  console.log("Received event:", JSON.stringify(event));
+
   const method = event.httpMethod;
-  const url = event.queryStringParameters.url;
+  const originalUrl = event.queryStringParameters.url;
+  const parsedUrl = new URL(originalUrl);
   const headers = event.headers || {};
   const body = event.body || null;
 
-  // Remove the 'Host' header from the incoming headers
-  const { Host, ...proxyHeaders } = headers;
+  // Remove the 'url' key from the queryStringParameters
+  const { url, ...otherParams } = event.queryStringParameters;
 
-  const makeRequest = async (url, options) => {
-    try {
-      const response = await axios({
-        url: url,
-        method: options.method,
-        headers: options.headers,
-        data: options.body,
-        responseType: "arraybuffer", // Handle binary content
-      });
-
-      return {
-        status: response.status,
-        headers: response.headers,
-        data: response.data,
-      };
-    } catch (error) {
-      if (error.response) {
-        throw {
-          status: error.response.status,
-          headers: error.response.headers,
-          message: error.response.data,
-        };
-      } else {
-        throw error;
-      }
-    }
+  // Combine the original URL's query parameters and the additional parameters
+  const allParams = {
+    ...otherParams,
+    ...Object.fromEntries(parsedUrl.searchParams),
   };
+  parsedUrl.search = new URLSearchParams(allParams).toString();
 
-  const rewriteUrls = (content, baseUrl, proxyUrl) => {
-    return content.replace(
-      /(href|src)=("|')([^"']*)(("|'))/gi,
-      (match, attribute, openQuote, originalUrl, closeQuote) => {
-        try {
-          const parsedUrl = new URL(originalUrl, baseUrl);
-          const proxyPath = `${proxyUrl}?url=${encodeURIComponent(
-            parsedUrl.href
-          )}`;
-          return `${attribute}=${openQuote}${proxyPath}${closeQuote}`;
-        } catch (error) {
-          return match;
-        }
-      }
-    );
+  const { Host, ...proxyHeaders } = headers;
+  const axiosOptions = {
+    url: parsedUrl.toString(),
+    method: method,
+    headers: proxyHeaders,
+    data: body,
+    responseType: "arraybuffer", // Handle binary content
   };
 
   try {
-    const options = {
-      method: method,
-      headers: proxyHeaders,
-      body: body,
-    };
+    const response = await axios(axiosOptions);
 
-    const response = await makeRequest(url, options);
+    const isTextBased = /^text\/|application\/(xml|json)/.test(
+      response.headers["content-type"]
+    );
 
-    // Check if the content is text-based before rewriting URLs
-    const contentType = response.headers["content-type"];
-    const isTextBased =
-      contentType && /^text\/|application\/(xml|json)/.test(contentType);
-
-    if (isTextBased) {
-      const proxyUrl =
-        "https://7d4eeq7hb7.execute-api.us-west-1.amazonaws.com/prod/proxy";
-      response.data = rewriteUrls(
-        response.data.toString("utf-8"),
-        url,
-        proxyUrl
-      );
-    }
-
-    const apiGatewayResponse = {
+    const result = {
       statusCode: response.status,
       headers: response.headers,
       body: isTextBased
-        ? response.data
-        : Buffer.from(response.data, "binary").toString("base64"), // Encode binary content as base64
+        ? response.data.toString("utf-8")
+        : Buffer.from(response.data, "binary").toString("base64"),
       isBase64Encoded: !isTextBased,
     };
-
-    return apiGatewayResponse;
+    console.log("Returning result:", JSON.stringify(result));
+    return result;
   } catch (error) {
-    const apiGatewayErrorResponse = {
-      statusCode: error.status || 500,
-      headers: error.headers || {},
-      body: error.message || "An error occurred",
-    };
-
-    return apiGatewayErrorResponse;
+    console.error("Error processing request:", error);
+    if (error.response) {
+      return {
+        statusCode: error.response.status,
+        headers: error.response.headers,
+        body: error.response.data.toString("utf-8"),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: "An error occurred",
+      };
+    }
   }
 };
